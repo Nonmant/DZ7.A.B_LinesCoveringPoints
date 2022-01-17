34e6b0db030395cfb87329a070230ea5d18e9402
Anton Bolotskih <89161424838@yandex.ru>
2022-01-17 22:34:24 +0300

    Initial, WA on 6 test

---
+++ funcs.cpp
@@ -0,0 +1,52 @@
+//https://contest.yandex.ru/contest/27883/problems/B/
+
+#include "funcs.h"
+#include <bits/stdc++.h>
+
+static const short int LineStart = 0;
+static const short int Point = 1;
+static const short int LineEnd = 2;
+typedef short int EventType;
+
+void parseFile(std::istream & input, std::ostream & output){
+
+    int nLines, mPoints;
+    input >> nLines >> mPoints;
+
+    //! @brief coordinate, eventType, point index
+    std::vector<std::tuple<int,EventType,int>> events(nLines*2 + mPoints);
+    std::vector<int> points(mPoints);
+
+    int start, end;
+    for(int i = 0; i<nLines; ++i){
+        input >> start >> end;
+        events[i*2] = {start, LineStart, -1};
+        events[i*2 + 1] = {end, LineEnd, -1};
+    }
+
+    for(int i = 0; i<mPoints; ++i){
+        input >> start;
+        events[i+nLines*2] = {start,Point,i};
+    }
+
+    std::sort(events.begin(), events.end());
+    int lines = 0;
+    for(const auto & event : events){
+        switch(std::get<1>(event)){
+            case LineStart:{
+                ++lines;
+                break;
+            }
+            case Point:{
+                points[std::get<2>(event)] = lines;
+                break;
+            }
+            case LineEnd:{
+                --lines;
+                break;
+            }
+        }
+    }
+
+    std::copy(points.begin(), points.end(), std::ostream_iterator<int>(output, " "));
+}
---
+++ test.cpp
@@ -0,0 +1,37 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+#include "catch.hpp"
+
+TEST_CASE("Catch operability", "[simple]"){
+    REQUIRE(1+1 == 2);
+}
+
+#include "funcs.h"
+
+TEST_CASE("test 1, file", "[simple]"){
+    std::ofstream inputPrepare;
+    inputPrepare.open ("input.txt", std::ofstream::trunc);
+    inputPrepare<<
+                    "3 2\n"
+                    "0 5\n"
+                    "-3 2\n"
+                    "7 10\n"
+                    "1 6\n"
+            ;
+    inputPrepare.close();
+
+    std::ifstream input( "input.txt", std::ofstream::in);
+    std::ofstream output("output.txt", std::ofstream::trunc);
+    parseFile(input,output);
+    input.close();
+    output.close();
+
+    std::ifstream outputCheck("output.txt", std::ofstream::in);
+    std::stringstream buffer;
+    buffer<<outputCheck.rdbuf();
+    outputCheck.close();
+    INFO(buffer.str());
+    REQUIRE(buffer.str() ==
+            "2 0 "
+    );
+}
